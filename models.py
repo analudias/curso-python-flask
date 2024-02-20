5c2e72537cc075b9b56f789065bc3b71ccaeffc9
Ana Luísa Dias <analudias@live.com>
2024-02-19 22:57:11 -0300

    Configurações de teste para BD

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class Usuario(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    username = database.Column(database.String, nullale=False)
+    username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg')
@@ -12,7 +12,7 @@ class Usuario(database.Model):
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    titulo = database.Column(database.String, nullale=False)
-    corpo = database.Column(database.Text, nullale=False)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
-    id_usuario = database.Column(database.Integer, database.ForeingKey('usuario.id'), nullable=False)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
---
+++ testes.py
@@ -0,0 +1,42 @@
+from main import app, database
+from models import Usuario, Post
+
+# with app.app_context():
+#     database.create_all()
+
+# with app.app_context():
+#     usuario = Usuario(username="Lira", email="lira@gmail.com", senha=123456)
+#     usuario2 = Usuario(username="Joao", email="joao@gmail.com", senha=123456)
+
+#     database.session.add(usuario)
+#     database.session.add(usuario2)
+
+#     database.session.commit()
+
+# with app.app_context():
+#     meus_usuarios = Usuario.query.all()
+#     print(meus_usuarios)
+#     primeiro_usuario =  Usuario.query.first()
+#     print(primeiro_usuario.id)
+#     print(primeiro_usuario.email)
+#     print(primeiro_usuario.posts)
+#     print("******")
+#     usuario_teste = Usuario.query.filter_by(email='lira@gmail.com').first()
+#     print(usuario_teste)
+#     print(usuario_teste.username)
+
+# with app.app_context():
+#     meu_post = Post(id_usuario=1, titulo="Meu primeiro post", corpo='Corpo do primeiro post')
+
+#     database.session.add(meu_post)
+
+#     database.session.commit()
+
+# with app.app_context():
+#     post = Post.query.first()
+#     print(post.titulo)
+#     print(post.autor.email)
+
+with app.app_context():
+    database.drop_all()
+    database.create_all()
